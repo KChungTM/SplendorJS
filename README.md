4bb8aae3305e196c113b2d5c9d5829e411022d3e
Kennedy <kennedychung24@gmail.com>
2020-12-11 03:53:36 -0800

    created cardWriter
    created a program that takes user input and creates cards for later use in the form of a JSON.

--- README.md
+++ README.md
@@ -3,7 +3,11 @@
  
  Hello to whoever may be reading this! This is a quick project I thought I'd make to test my newly learned JavaScript, HTML, and CSS skills. I made a copy of my favorite board game, "Splendor", that could be played through the browser locally with another person. Unfortunately, I haven't found a way to play online with another person yet but maybe that will be something I add on a later date...
  
- So far I've implemented turn by turn token selection, an end turn button, and an undo button. I still have a lot of work to do with implementing cards, reserving, score checking, noble checking, etc.
+ So far I've implemented turn by turn token selection, an end turn button, and an undo button. As of my last update, I created a program to enter the card information into, which stores the data as a JSON file that I will access later. I still have a lot of work to do with implementing cards, reserving, score checking, noble checking, etc.
+
+ TAKEAWAYS: HTML: Using Bootstrap, Refresher 
+            CSS: Refresher 
+            JavaScript: Promises, Async/Await, JSON Data Storage, Object Manipulation
  
  P.S: I hope the window works ok for whoever decides to play it. I looks fine on my screen but my knowledge of breakpoints and sizing HTML elements proportionally is pretty abysmal so knock yourself out.
  
---
+++ cardWrite/writer.js
@@ -0,0 +1,272 @@
+// IMPORTS
+// =======
+// Import File System
+const { RSA_X931_PADDING } = require("constants");
+const fs = require("fs");
+const { resolve } = require("path");
+// Import Line Reader and Initialize Necessary Stuff
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// MAIN FUNCTION THAT CALLS CARDS/NOBLES
+async function splendorWrite(){
+    // COMPILE DECK AND NOBLES
+    deck = await gatherCardValues();
+    nobles = await gatherNobles();
+
+    // COMBINE DECK/NOBLE AND WRITE TO JSON
+    writeToJSON(deck, nobles)
+    console.log("=========================================")
+    console.log("DONE CREATING MASTERDECK JSON............");
+    console.log("=========================================\n\n\n")
+    rl.close();
+}
+
+// FUNCTION TO COLLECT NOBLE VALUES
+async function gatherNobles(){
+    console.log("Splendor Noble Writer\n======================")
+    console.log("Cost: Cost in Cards (<BLUE> <RED> <GREEN> <BROWN> <WHITE>)");
+    console.log("(Please Type \"Done\" After You Are Finished...)")
+    console.log("=========================================================");
+    rl.pause();
+
+    //INIT NOBLE DECK
+    const nobleDeck = {nobles:[]};
+    let nobleInfo = true;
+    console.log(nobleDeck.nobles);
+    console.log("CHECKPOINT 1");
+
+    // ENTER INFO TO CREATE NOBLES
+    while(nobleInfo){
+        await getNobleInfo().then(values => {
+            nobleInfo = values;
+        })
+        if(nobleInfo === "invalid" ||!nobleInfo)
+            continue;
+        else{
+            nobleDeck.nobles.push(new noble(nobleInfo[0]));
+        }
+    }
+
+    console.log("==========================================")
+    console.log("Finished Compiling the Nobles.............");
+    console.log("==========================================\n\n\n")
+
+    return nobleDeck;
+}
+
+// FUNCTION TO COLLECT CARD VALUES
+async function gatherCardValues(){
+    console.log("Splendor Card Writer\n====================")
+    console.log("Level: Refers to Card Tier (E.G. BLUE:3 YELLOW:2 GREEN:1)");
+    console.log("Cost: Cost in Gems (<BLUE> <RED> <GREEN> <BROWN> <WHITE>)");
+    console.log("(Please Type \"Done\" After You Are Finished...)")
+    console.log("=========================================================");
+    rl.pause();
+
+    // INIT CARD DECK
+    const cardDeck = {tier1:[],tier2:[],tier3:[]}
+    let cardInfo = true;
+
+    // ENTER INFO TO CREATE CARDS
+    while(cardInfo){
+        await getCardInfo().then(values => {
+            cardInfo = values;
+        });
+        // CHECKS FOR INVALID CARDS
+        if(cardInfo === "invalid")
+            continue;
+
+        // ASSIGNS CARDS BASED ON LEVEL
+        switch(cardInfo[1]){
+            case(1):
+                cardDeck.tier1.push(new card(cardInfo[0],cardInfo[1],cardInfo[2],cardInfo[3]));
+                break;
+            case(2):
+                cardDeck.tier2.push(new card(cardInfo[0],cardInfo[1],cardInfo[2],cardInfo[3]));
+                break;
+            case(3):
+                cardDeck.tier3.push(new card(cardInfo[0],cardInfo[1],cardInfo[2],cardInfo[3]));
+                break;
+            default:
+                // CASE WHEN USER INPUTS "Done"
+                console.log("=========================================")
+                console.log("Finished Compiling the Deck..............");
+                console.log("=========================================\n\n\n")
+        }
+    }
+    return cardDeck;
+}
+
+// GETS THE NOBLE INFO USERS SUBMIT
+async function getNobleInfo(){
+    rl.resume();
+    let nobleInfo = [];
+
+    return new Promise((resolve, reject) => {
+        rl.question("Cards Needed?: ", function(cost){
+            if(cost.trim().toLowerCase() === "done"){
+                resolve(false);
+                rl.pause();
+            }
+            else{
+                cost = cost.trim().split(/\s+/).map(token => parseInt(token));
+                if(notValidNoble(cost)){
+                    console.log(`[INVALID NOBLE COST: ${cost}]`);
+                    resolve("invalid");
+                }
+                else{
+                    nobleInfo.push(cost);
+                    console.log(nobleInfo);
+                    rl.pause();
+                    resolve(nobleInfo);
+                }
+            }
+        });
+    });
+}
+
+// GETS THE CARD INFO USERS SUBMIT
+async function getCardInfo(){
+    rl.resume();
+    let cardInfo = [];
+
+    return new Promise((resolve, reject) => {
+        rl.question("Card Color?: ", function(color){
+            if(color.trim().toLowerCase() === "done"){
+                resolve(false);
+                rl.pause();
+            }
+            else if(notValidColor(color)){
+                console.log(`[INVALID COLOR: ${color}]`);
+                resolve("invalid")
+            }
+            else{
+                rl.question("Card Level?: ", function(level){
+                    if(notValidLevel(parseInt(level))){
+                        console.log(`[INVALID LEVEL: ${level}]`);
+                        resolve("invalid");
+                    }
+                    else{
+                        rl.question("Gem Cost? ", function(cost){
+                            cost = cost.trim().split(/\s+/).map(token => parseInt(token))
+                            if(notValidCost(cost)){
+                                console.log(`[INVALID COST: ${cost}]`);
+                                resolve("invalid");
+                            }
+                            else{
+                                rl.question("Points Earned: ", function(points){
+                                    if(notValidPoint(parseInt(points))){
+                                        console.log(`[INVALID POINTS: ${points}]`);
+                                        resolve("invalid");
+                                    }
+                                    else{
+                                        cardInfo.push(color.trim()); cardInfo.push(parseInt(level)); cardInfo.push(cost); cardInfo.push(parseInt(points));
+                                        console.log(cardInfo);
+                                        rl.pause();
+                                        resolve(cardInfo);
+                                    }
+                                });
+                            }
+                        });
+                    }
+                });
+            }
+        });
+    });
+}
+
+// CHECKS IF NOBLE IS VALID
+function notValidNoble(cost){
+    if(cost.length === 5){
+        cost.forEach(cardCost => {
+            if(cardCost < 0 || cardCost > 4)
+                return true;
+        });
+        return false;
+    }
+    return true; 
+}
+
+// CHECKS IF COLOR IS VALID
+function notValidColor(color){
+    if(["blue","red","green","brown","white"].includes(color.trim().toLowerCase()))
+        return false;
+    return true;
+}
+
+// CHECKS IF LEVEL IS VALID
+function notValidLevel(level){
+    if(level > 3 || level < 0)
+        return true;
+    return false;
+}
+
+// CHECKS IF COST IS VALID
+function notValidCost(cost){
+    if(cost.length === 5){
+        cost.forEach((gemCost) => {
+            if(gemCost < 0 || gemCost > 7)
+                return true;
+        });
+        return false;
+    }
+    return true;
+}
+
+// CHECKS IF POINTS ARE A NUMBER
+function notValidPoint(points){
+    if(isNaN(points)){
+        return true;
+    }
+    return false;
+}
+
+// FUNCTION TO WRITE JSON
+function writeToJSON(masterDeck, nobles){
+    // COMBINE MY NOBLES KEY WITH DECK OBJECT
+    masterDeck["nobles"] = nobles["nobles"];
+
+    console.log("=========================================")
+    console.log("STRINGIFYING THE JS.....................");
+    console.log("=========================================\n\n\n")
+
+    // STRINGIFY MY DECK AND USE FUNCTION TO MAKE IT PRETTY
+    let jsonVer = JSON.stringify(masterDeck,function(a,b){
+        if(a === "cost" || b === "nobles")
+            return JSON.stringify(b);
+        return b;
+    },"\t").replace(/\\/g, '')
+    .replace(/\"\[/g, '[')
+    .replace(/\]\"/g,']')
+    .replace(/\"\{/g, '{')
+    .replace(/\}\"/g,'}');
+
+    console.log("=========================================")
+    console.log("WRITING THE JSON FILE....................");
+    console.log("=========================================\n\n\n")
+
+    // WRITES IN "MasterDeck.json"
+    fs.writeFile("masterDeck.json", jsonVer, function(err, result) {
+        if(err) console.log('error', err);
+    }); 
+}
+
+// CARD OBJECT
+function card(color, level, cost, points){
+    this.color = color;
+    this.level = level;
+    this.cost = cost;
+    this.points = points;
+}
+
+// NOBLE OBJECT
+function noble(cost){
+    this.cost = cost,
+    this.points = 3
+}
+
+splendorWrite();
